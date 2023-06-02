def5cdc8b1b0322a241e8c5adef2ee7c0dedae3e
zhuhaojie <zhuhaojie@cqslim.com>
2023-06-02 12:17:23 +0800

    feat: 删除theme-chalk

--- example/main.ts
+++ example/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from './app.vue'
 import CQButton from "@cq/components/button";
 import CQIcon from "@cq/components/icon";
-import '@cq/theme-chalk/index.scss'
 
 const app = createApp(App)
 
